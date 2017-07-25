2fe4c257b41c53716c5d09a3bcac3b33bb1cf214
alexpts <alexpts@yandex.ru>
2017-07-25 09:51:58 +0300

    up min version php to 7.1

--- src/PTS/SymfonyAsset/LastModifyStrategy.php
+++ src/PTS/SymfonyAsset/LastModifyStrategy.php
@@ -14,7 +14,7 @@ class LastModifyStrategy implements VersionStrategyInterface
      * @param string $staticDir
      * @param string|null $cdnHost
      */
-    public function __construct($staticDir, $cdnHost = null)
+    public function __construct(string $staticDir, string $cdnHost = null)
     {
         $this->staticDir = $staticDir;
         $this->cdnHost = $cdnHost;
@@ -23,7 +23,7 @@ class LastModifyStrategy implements VersionStrategyInterface
     /**
      * @inheritdoc
      */
-    public function getVersion($path)
+    public function getVersion($path): string
     {
         $path = $this->staticDir . '/' . $path;
         return file_exists($path) ? (string)filemtime($path) : '';
@@ -32,7 +32,7 @@ class LastModifyStrategy implements VersionStrategyInterface
     /**
      * @inheritdoc
      */
-    public function applyVersion($relPath)
+    public function applyVersion($relPath): string
     {
         $version = $this->getVersion($relPath);
         $path = $version ? sprintf('%s?v=%s', $relPath, $version) : $relPath;
